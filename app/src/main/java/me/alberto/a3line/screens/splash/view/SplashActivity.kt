409d02c42ba50b14a605a11faf0019299d0f1605
albertakpos <albertakpos@gmail.com>
2021-01-19 17:44:13 +0100

    initial commit

---
+++ app/src/main/java/me/alberto/a3line/screens/splash/view/SplashActivity.kt
@@ -0,0 +1,12 @@
+package me.alberto.a3line.screens.splash.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.alberto.a3line.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
